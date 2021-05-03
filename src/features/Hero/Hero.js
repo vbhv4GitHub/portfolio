02bf4e497b55a5516590de04fc1bb24c77859709
vbhv4991GitHub <ravi9406126068@gmail.com>
2021-05-03 13:33:11 +0530

    Design for hero section is ready, need to fix font sizing.

--- src/features/Hero/Hero.js
+++ src/features/Hero/Hero.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Hero.css';
-// import { Link } from 'react-router-dom';
+// import &rcub; Link } from 'react-router-dom';
 // import { motion } from 'framer-motion';
 // import { variants } from '../PageTransitions/PageTransitions';
 
@@ -14,9 +14,37 @@ const Hero = () => {
             <div className='hero__image'></div>
             <div className='hero__intro'>
                <h1>
-                  Full-Stack
+                  <span className='blue'>function</span>
+                  <span className='light-yellow'> main</span>
+                  <span className='yellow'>(</span>
+                  <span className='light-blue'>alive</span>
+                  <span className='yellow'>) {' {'}</span>
                   <br />
-                  <span>Web Developer</span>
+                  &nbsp;&nbsp;&nbsp;&nbsp;
+                  <span className='light-blue'> eat</span>
+                  <span className='pink'>(</span>
+                  <span className='pink'>)</span>;
+                  <br />
+                  &nbsp;&nbsp;&nbsp;&nbsp;
+                  <span className='light-blue'> sleep</span>
+                  <span className='pink'>(</span>
+                  <span className='pink'>)</span>;
+                  <br />
+                  &nbsp;&nbsp;&nbsp;&nbsp;
+                  <span className='light-blue'> code</span>
+                  <span className='pink'>(</span>
+                  <span className='pink'>)</span>;
+                  <br />
+                  &nbsp;&nbsp;&nbsp;&nbsp;
+                  <span className='pink'>if (</span>
+                  <span className='light-blue'>alive</span>
+                  <span className='pink'>)</span>
+                  <span className='light-yellow'> main</span>
+                  <span className='pink'>(</span>
+                  <span className='light-blue'>alive</span>
+                  <span className='pink'>)</span>
+                  ; <br />
+                  <span className='yellow'>{'}'}</span>
                </h1>
                <button className='btn btn-neon btn-xs'>Read More</button>
             </div>
